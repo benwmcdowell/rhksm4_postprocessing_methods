865c93b58a32d1c7c306a1822565c7434bde919a
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2021-09-17 12:20:08 -0700

    Update plot_single_point_spectra.py
    Modified values returned by function that extracts data from rhksm4 class

--- plot_single_point_spectra.py
+++ plot_single_point_spectra.py
@@ -6,13 +6,18 @@ import numpy as np
 def get_single_point(ifile):
         
     f=rhksm4.load(ifile)
-    ydata=f[0].data[0]*f[0].attrs['RHK_Zscale']+f[0].attrs['RHK_Zoffset']
-    xdata=[f[0].attrs['RHK_Xoffset']+i*f[0].attrs['RHK_Xscale'] for i in range(len(ydata))]
+    ydata=f[0].data[0]*f[0].attrs['RHK_Zscale']+f[0].attrs['RHK_Zoffset'] #LIA current in pA
+    xdata=[f[0].attrs['RHK_Xoffset']+i*f[0].attrs['RHK_Xscale'] for i in range(len(ydata))] #bias in V
+    setpoint=f[0].attrs['RHK_Current']/1e-12 #current setpoint in pA
     
     #converts current data to pA
     ydata/=1e-12
     
-    return xdata,ydata
+    #reverses data to be in logical bias progression
+    xdata=xdata[::-1]
+    ydata=ydata[::-1]
+    
+    return xdata,ydata,setpoint
 
 def plot_single_point(ifiles,labels,**args):
     if 'normalize' in args:
@@ -21,31 +26,33 @@ def plot_single_point(ifiles,labels,**args):
     else:
         normalize=False
         
-    if 'fit' in args:
-        fit=True
+    if 'fano' in args:
+        fano=True
     else:
-        fit=False
+        fano=False
         
     ydata=[]
     xdata=[]
+    setpoints=[]
     num=len(ifiles)
     for i in ifiles:
         tempvar=get_single_point(i)
         xdata.append(tempvar[0])
         ydata.append(tempvar[1])
+        setpoints.append(tempvar[2])
         if normalize:
             ydata[-1]/=max(ydata[-1])
     
-    if fit:
+    if fano:
         fit_params=[]
         for i in range(num):
-            fit_params.append(curve_fit(fano_fit,xdata[i][40:],ydata[i][40:],p0=[-5,2.4,0.5],bounds=([-200,2.2,0.05],[-.5,2.6,0.6]))[0])
+            fit_params.append(curve_fit(fano_fit,xdata[i],ydata[i],p0=[-5,2.6,0.1],bounds=([-20,2.2,0.05],[0,2.9,1.0]))[0])
         print(fit_params)
             
     plt.figure()
     for i in range(num):
         plt.scatter(xdata[i],ydata[i],label=labels[i])
-        if fit:
+        if fano:
             plt.plot(xdata[i],fano_fit(xdata[i],fit_params[i][0],fit_params[i][1],fit_params[i][2]))
     plt.ylabel('LIA current / pA')
     plt.xlabel('bias / V')

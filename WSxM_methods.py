747273f7ca07717a30657d66b47cdc823a89311c
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2022-01-27 13:38:29 -0800

    Update WSxM_methods.py
    Added options for smoothing and fitting profile files

--- WSxM_methods.py
+++ WSxM_methods.py
@@ -1,4 +1,8 @@
-
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
 
 class profile():
     def __init__(self,ifile):
@@ -12,7 +16,34 @@ class profile():
                     break
                 if read_data:
                     line=line.split()
-                    self.x.append(line[0])
-                    self.y.append(line[1])
+                    self.x.append(float(line[0]))
+                    self.y.append(float(line[1]))
                 if '[Header end]' in line:
                     read_data=True
+        self.x=np.array(self.x)
+        self.y=np.array(self.y)
+        
+    def plot_profile(self):
+        self.fig,self.ax=plt.subplots(1,1)
+        
+    def add_savgol_filter(self,w,o):
+        self.y=savgol_filter(self.y,w,o)
+        
+    def add_gaussian_filter(self,s):
+        self.y=gaussian_filter(self.y,s)
+    
+    def fit_peak(self,**args):
+        def gauss(x,a,x0,s,y0):
+            y=y0+a*np.exp(-(x-x0)**2/s/2)
+            return y
+        
+        if 'xrange' in args:
+            xrange=args['xrange']
+            for i in range(2):
+                xrange[i]=np.argmin(abs(self.x-xrange[i]))
+        else:
+            xrange=[0,len(self.x)]
+        
+        popt,pcov=curve_fit(gauss,self.x[xrange[0]:xrange[1]],self.y[xrange[0]:xrange[1]],p0=[max(self.y[xrange[0]:xrange[1]]),np.average(self.x[xrange[0]:xrange[1]]),0.05,0],bounds=([0,np.min(self.x[xrange[0]:xrange[1]]),0,0],[np.max(self.y[xrange[0]:xrange[1]])*1.5,np.max(self.x[xrange[0]:xrange[1]]),self.x[-1]-self.x[0],np.max(self.y[xrange[0]:xrange[1]])]))
+        pcov=np.sqrt(np.diag(pcov))
+        print('peak is centered at {} +/- {}'.format(popt[1],pcov[1]))
